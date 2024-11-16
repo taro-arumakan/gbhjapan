0ebeaf144286d1f67cae1d7e4797935a7773417f
taro <taro.arumakan@gmail.com>
2024-11-16 11:53:35 +0900

    animate articles

---
+++ assets/animate_article.js
@@ -0,0 +1,17 @@
+['h1', 'h2', 'h3'].forEach(tag => {
+  document.querySelectorAll(tag).forEach(element => {
+    element.classList.add('reveal_tran_bt');
+  });
+});
+
+const transitionsMap = {
+  0: 'reveal_tran_bt',
+  1: 'reveal_tran_lr',
+  2: 'reveal_tran_rl',
+  3: 'reveal_tran_tb'
+};
+const keys = Object.keys(transitionsMap).length;
+document.querySelectorAll('.prose>div').forEach((element, index) => {
+  const tran = transitionsMap[index % keys];
+  element.classList.add(tran);
+});
