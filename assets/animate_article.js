ea77c8be7a3fc80fbc6c9758eed1f49e359ef7c8
taro-arumakan <taro.arumakan@gmail.com>
2024-11-18 10:51:26 +0900

    .h6 as well

--- assets/animate_article.js
+++ assets/animate_article.js
@@ -5,7 +5,7 @@ const transitionsMap = {
   3: 'reveal_tran_tb'
 };
 const keys = Object.keys(transitionsMap).length;
-document.querySelectorAll('h1, h2, h3, .prose div, .prose p').forEach((element, index) => {
+document.querySelectorAll('h1, h2, h3, .h6, .prose div, .prose p').forEach((element, index) => {
   const tran = transitionsMap[index % keys];
   element.classList.add(tran);
 });
